5c830e6c33b936c0232108c863b40963473937bc
José Ulisses <julissesmacedo@outlook.com>
2023-11-15 19:50:31 -0300

    Configurando autorização e swagger

--- MsEmail.API/Program.cs
+++ MsEmail.API/Program.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using MsEmail.API.Context;
-using MsEmail.API.Controllers;
-using System.Configuration;
+using System.Text;
 
 namespace MsEmail.API
 {
@@ -16,15 +16,64 @@ namespace MsEmail.API
 
             var connectionString = builder.Configuration.GetConnectionString("MsEmail");
             //builder.Services.AddDbContext<EmailContext>(db => db.UseInMemoryDatabase("Emails"));
-            builder.Services.AddDbContext<EmailContext>(db => db.UseSqlServer(connectionString));
+            builder.Services.AddDbContext<AppDbContext>(db => db.UseSqlServer(connectionString));
 
             builder.Services.Configure<SmtpConfiguration>(builder.Configuration.GetSection("SmtpConfiguration"));
             builder.Services.Configure<TokenConfiguration>(builder.Configuration.GetSection("Token"));
 
             builder.Services.AddControllers();
+
+            var key = Encoding.UTF8.GetBytes(builder.Configuration.GetSection("Token").GetValue<string>("Secrety"));
+
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(x =>
+                {
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                    };
+                });
+
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "apiagenda", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer'[space] and then your token in the text input below. \r\n\r\nExample: \"Bearer 12345abcdef\"",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                         new string[] {}
+                    }
+                });
+            });
 
             var app = builder.Build();
 
