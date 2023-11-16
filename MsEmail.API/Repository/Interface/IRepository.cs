07ab7b6db7d9f122743fa6a66f01083e1e3b599c
José Ulisses <julissesmacedo@outlook.com>
2023-11-16 01:26:22 -0300

    Renomeando repository generico

---
+++ MsEmail.API/Repository/Interface/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using MsEmail.API.Context;
+using MsEmail.API.Entities;
+using System.Linq.Expressions;
+
+namespace MsEmail.API.Repository.Interface
+{
+    public interface IRepository<T> : IDisposable
+    {
+        public void Save();
+        public IRepository<T> Insert(T entity);
+        public IRepository<T> Update(T entity);
+        public IRepository<T> Delete(long id);
+        public IRepository<T> InsertRange(List<T> entitys);
+        public IEnumerable<T> GetAll();
+        public T? GetById(long id);
+        public IEnumerable<T> Find(Expression<Func<T, bool>> expression);
+    }
+}
