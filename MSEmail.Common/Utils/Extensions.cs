b39c3f5ef5e695f7657c0600db581b64d0f25efb
José Ulisses <julissesmacedo@outlook.com>
2023-11-15 21:45:55 -0300

    Adicionando extensão para pegar o id e o cargo

--- MSEmail.Common/Utils/Extensions.cs
+++ MSEmail.Common/Utils/Extensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +10,15 @@ public static class Extensions
     public static bool IsNull(this object obj)
     {
         return obj == null;
+    }
+
+    public static long GetUserID(this ClaimsPrincipal User)
+    {
+        return long.Parse(User.Claims.First(i => i.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value);
+    }
+
+    public static string GetRole(this ClaimsPrincipal User)
+    {
+        return User.Claims.First(i => i.Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/role").Value;
     }
 }
