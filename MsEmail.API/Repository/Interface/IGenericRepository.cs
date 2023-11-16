dd1e9f07281ee18899466ea92d1eadc8b975d827
José Ulisses <julissesmacedo@outlook.com>
2023-11-15 23:38:31 -0300

    Criando UserRepository

---
+++ MsEmail.API/Repository/Interface/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using MsEmail.API.Entities;
+
+namespace MsEmail.API.Repository.Interface
+{
+    public interface IGenericRepository<T> : IDisposable
+    {
+        public IEnumerable<T> GetAll();
+        public T? GetById(int id);
+        public IGenericRepository<T> Insert(T entity);
+        public IGenericRepository<T> Delete(int id);
+        public IGenericRepository<T> Update(T entity);
+        public void Save();
+    }
+}
---
+++ MsEmail.API/Repository/UserRepository.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MsEmail.API.Context;
+using MsEmail.API.Entities;
+using MsEmail.API.Repository.Interface;
+using NuGet.Protocol.Plugins;
+
+namespace MsEmail.API.Repository
+{
+    public class UserRepository : IGenericRepository<User>, IDisposable
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+
+        public User? GetById(int id)
+        {
+            return _context.Users.Find(id);
+        }
+
+        public IGenericRepository<User> Insert(User entity)
+        {
+            entity.CreationDate = entity.UpdateDate = DateTime.Now;
+            _context.Users.Add(entity);
+            return this;
+        }
+
+        public IGenericRepository<User> Update(User entity)
+        {
+            entity.UpdateDate = DateTime.Now;
+            _context.Users.Update(entity);
+            return this;
+        }
+
+
+        public User? GetByLogin(string email, string password)
+        {
+            return _context.Users.FirstOrDefault(x => x.Email == email && password == x.Password);
+        }
+
+        public IGenericRepository<User> Delete(int id)
+        {
+            User user = _context.Users.Find(id);
+            user.DeletionDate = DateTime.Now;
+            _context.Users.Remove(user);
+            return this;
+        }
+
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
