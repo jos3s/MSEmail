a1094011c165e059bfd0f0dc7ea9b0b322cb82d3
José Ulisses <julissesmacedo@outlook.com>
2023-11-20 23:21:05 -0300

    Buscando emails e enviando #20

--- MSEmail.Sender/Transaction/ExecuteTRA.cs
+++ MSEmail.Sender/Transaction/ExecuteTRA.cs
@@ -1,4 +1,8 @@
+﻿using MsEmail.Domain.Entities;
 ﻿using MsEmail.Infra.Context;
+using MsEmail.Sender.Service;
+using MSEmail.Domain.Enums;
+using MSEmail.Infra.Business;
 using MSEmail.Infra.Repository;
 
 namespace MSEmail.Sender.Transaction;
@@ -6,21 +10,44 @@ namespace MSEmail.Sender.Transaction;
 public class ExecuteTRA
 {
     private readonly EmailRepository _emailRepository;
+    private readonly CommonLog _commonLog;
+    private readonly AppDbContext _context;
 
     public ExecuteTRA(AppDbContext context)
     {
+        _context = context;
         _emailRepository = new EmailRepository(context);
+        _commonLog = new CommonLog(context);
     }
 
     public void Execute()
     {
         try
         {
+            List<Email> emails = _emailRepository.GetEmailsByStatus(EmailStatus.Created);
 
+            foreach (Email item in emails)
+            {
+                Send(item);
+            }
         }
         catch (Exception ex)
         {
+            _commonLog.SaveExceptionLog(ex, nameof(Execute), this.GetType().Name, ServiceType.Microservice);
+        }
+    }
 
+    private void Send(Email email)
+    {
+        try
+        {
+            EmailService.Send(email);
+
+            _emailRepository.Save();
+        }
+        catch (Exception ex)
+        {
+            _commonLog.SaveExceptionLog(ex, nameof(Send), this.GetType().Name, ServiceType.Microservice);
         }
     }
 }
