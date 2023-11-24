6d5f8667412289daf5de3e3abf6284d21d448d50
José Ulisses <julissesmacedo@outlook.com>
2023-11-23 22:09:23 -0300

    Adicionando Model de retorno para listas de emails #26

---
+++ MsEmail.API/Models/Email/ListEmailModel.cs
@@ -0,0 +1,11 @@
+﻿using MsEmail.API.Models.EmailModel;
+
+namespace MSEmail.API.Models.Email
+{
+    public class ListEmailModel
+    {
+        public int Count { get; set; }
+
+        public List<ViewEmailModel> Emails { get; set; }
+    }
+}
