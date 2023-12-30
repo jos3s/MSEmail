cb3d2d3efc754a6c73a8cf529ddbd3924f913356
José Ulisses <julissesmacedo@outlook.com>
2023-12-29 23:26:28 -0300

    Removendo WebUI

--- MSEmail.WebUI/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MSEmail.WebUI.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
