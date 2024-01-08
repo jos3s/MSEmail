b82adca04960e2cc0ee01478a3dd4ce6292b4f35
José Ulisses <julissesmacedo@outlook.com>
2024-01-07 21:27:13 -0300

    Removendo cache key generator desnecessário

--- MSEmail.Infra/Redis/CacheKey/TokenCacheKeyGenerator.cs
+++
@@ -1,8 +0,0 @@
-﻿using MSEmail.Domain.Entities.Redis;
-using MSEmail.Infra.Redis.CacheKey;
-
-namespace MSEmail.Infra.Redis.CacheKeyProviders;
-public class TokenCacheKeyGenerator : CacheKeyGenerator<Token>
-{
-    
-}
