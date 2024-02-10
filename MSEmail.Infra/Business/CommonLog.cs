79c262fc2d951f6de2c1cb133bbec3bd1abdb0fe
José Ulisses <julissesmacedo@outlook.com>
2024-02-10 18:45:55 -0300

    Adicionando salvamento do log ao salvar execeção

--- MSEmail.Infra/Business/CommonLog.cs
+++ MSEmail.Infra/Business/CommonLog.cs
@@ -1,25 +1,23 @@
 ﻿using MsEmail.Domain.Entities.Common;
 using MsEmail.Infra.Context;
+
 using MSEmail.Domain.Enums;
+using MSEmail.Infra.Log;
 using MSEmail.Infra.Repository;
 
 namespace MSEmail.Infra.Business;
 
 public class CommonLog
 {
-    private AppDbContext _context { get; set; }
     private ExceptionLogRepository _logs { get; set; }
 
-    public CommonLog(AppDbContext context)
-    {
-        _logs = new ExceptionLogRepository(context);
-    }
+    public CommonLog(AppDbContext context) => _logs = new ExceptionLogRepository(context);
 
     public void SaveExceptionLog(Exception ex, string method, string className, ServiceType serviceType, bool uniqueTransaction = true)
     {
         try
         {
-            ExceptionLog log = new ExceptionLog
+            ExceptionLog log = new ()
             {
                 Source = ex.Source,
                 StackTrace = ex.StackTrace,
@@ -28,14 +26,17 @@ public class CommonLog
                 ClassName = className,
                 ServiceType = serviceType
             };
+
+            LogSingleton.Instance.CreateExceptionLog(log);
+
             _logs.Insert(log);
 
             if (uniqueTransaction)
                 _logs.Save();
         }
-        catch (Exception)
+        catch (Exception exInternal) 
         {
-
+            LogSingleton.Instance.CreateExceptionLog(exInternal);
         }
     }
 }
